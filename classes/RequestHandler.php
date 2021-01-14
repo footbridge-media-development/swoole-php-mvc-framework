18d498b52547181cf9ec05de88e2a3205424e306
nox7 <17110935+nox7@users.noreply.github.com>
2021-01-14 07:58:16 -0600

    Init

---
+++ classes/RequestHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+	require_once __DIR__ . "/Router.php";
+
+	use Swoole\Http\Request;
+	use Swoole\Http\Response;
+
+	class RequestHandler{
+
+		/**
+		* Processes and routes a Swoole request
+		* @param Swoole\Http\Request $request
+		* @param Swoole\Http\Response $response
+		*/
+		public static function process(Router $router, Request $request, Response $response){
+			$serverData = $request->server;
+			$requestURI = $serverData['request_uri'];
+			$clientIP = $serverData['remote_addr'];
+			$requestType = $serverData['request_method'];
+
+			if ($requestType === "GET"){
+				$viewResponse = $router->route($requestURI, $request, $response);
+				if ($viewResponse !== null){
+					$response->end($viewResponse);
+				}else{
+					// TODO
+					// Static file?
+					$response->end("404\n");
+				}
+			}elseif ($requestType === "POST"){
+
+			}
+		}
+
+	}
---
+++ classes/Router.php
@@ -0,0 +1,49 @@
+<?php
+	use Swoole\Http\Request;
+	use Swoole\Http\Response;
+
+	class Router{
+
+		private $controllersFolder = "";
+		private $controllers = [];
+
+		/**
+		* Sets the controllers folder
+		*/
+		public function setControllersFolder(string $path){
+			$this->controllersFolder = $path;
+		}
+
+		/**
+		* Loads the MVC controller classes
+		* from the controllers folder
+		*/
+		public function loadMVCControllers(){
+			$fileNames = array_diff(scandir($this->controllersFolder), ['.','..']);
+
+			foreach ($fileNames as $controllerFileName){
+				// Ignore the base Controller
+				$controllerPath = sprintf("%s/%s", $this->controllersFolder, $controllerFileName);
+
+				// The class name _must_ be the file name minus the extension
+				$className = pathinfo($controllerFileName, PATHINFO_FILENAME);
+				require($controllerPath);
+				$thisController = new $className();
+				$this->controllers[] = $thisController;
+			}
+		}
+
+		/**
+		* @return string|null
+		*/
+		public function route(string $uri, Request $request, Response $response){
+			foreach ($this->controllers as $controller){
+				$routes = $controller->routes;
+				foreach ($routes as $route){
+					if ($uri === $route){
+						return $controller->getResult($request, $response);
+					}
+				}
+			}
+		}
+	}
